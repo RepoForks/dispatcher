3b7e1468b854ce59c5473def9b223502cfb87e57
Hendra Anggrian <hendraanggrian@gmail.com>
2017-04-12 17:49:32 +0700

    updated example

--- dispatcher-example/src/main/java/com/example/dispatcher/MainActivity.java
+++ dispatcher-example/src/main/java/com/example/dispatcher/MainActivity.java
@@ -5,11 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.dispatcher.collection.CaptureImageIntent;
 import com.example.dispatcher.collection.GetContentIntent;
 import com.example.dispatcher.collection.MimeType;
@@ -19,12 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Dispatcher";
 
+    private ViewGroup viewGroup;
     private ImageView imageViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewGroup = (ViewGroup) findViewById(R.id.viewgroup);
         imageViewResult = (ImageView) findViewById(R.id.imageview_result);
 
         Dispatcher.setDebug(true);
@@ -39,12 +42,8 @@ public class MainActivity extends AppCompatActivity {
                         })
                         .dispatch())
                 .onDenied(permissions -> Toast.makeText(MainActivity.this, "Permission denied!", Toast.LENGTH_SHORT).show())
-                .onShouldShowRationale((dispatcher, permissions) -> new MaterialDialog.Builder(this)
-                        .title("Permission to camera and storage are required.")
-                        .content("Click OK to request them again.")
-                        .cancelable(false)
-                        .positiveText(android.R.string.ok)
-                        .onPositive((dialog, which) -> dispatcher.dispatch())
+                .onShouldShowRationale((dispatcher, permissions) -> Snackbar.make(viewGroup, "Permission to camera and storage are required.", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Request", v1 -> dispatcher.dispatch())
                         .show())
                 .dispatch());
 
